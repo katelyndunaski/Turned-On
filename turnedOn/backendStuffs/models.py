f581b810f48da8cb48bc27c65a34462d83ebdcc3
Yoseph Radding <raddingy@lykeme.com>
2015-02-08 00:41:02 -0500

    added blank things

--- turnedOn/backendStuffs/models.py
+++ turnedOn/backendStuffs/models.py
@@ -8,9 +8,9 @@ class UserPhone(models.Model):
 	global regionChoices
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(validators=[phone_regex], unique=True, max_length = 10)
-	verificationNumber = models.IntegerField()
+	verificationNumber = models.IntegerField(blank = True)
 	name = models.CharField(max_length = 100)
-	token = models.IntegerField()
+	token = models.IntegerField(blank=True)
 	region = models.CharField(choices = regionChoices, max_length = 3)
 
 class UserinGroup(models.Model):
