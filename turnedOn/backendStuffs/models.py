0001eba72095430bdfdb898b973be66253a772b0
Yoseph Radding <raddingy@lykeme.com>
2015-02-07 23:20:21 -0500

    added User.name and Goups.isOn

--- turnedOn/backendStuffs/models.py
+++ turnedOn/backendStuffs/models.py
@@ -16,6 +16,7 @@ class UserPhone(models.Model):
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(validators=[phone_regex], unique=True, max_length = 10)
 	verificationNumber = models.IntegerField()
+	name = models.CharField(max_length = 100)
 	token = models.IntegerField()
 	region = models.ForeignKey(Regions)
 
@@ -25,7 +26,7 @@ class UserinGroup(models.Model):
 	user = models.ForeignKey(UserPhone)
 	name = models.CharField(max_length = 500)
 	description = models.TextField(blank = True)
-
+	isOn = models.BooleanField(default=True)
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	twilioNumber = models.CharField(validators=[phone_regex],max_length = 10)
 
