86387d9c006e04853bf5dced5196c1efc4368472
Yoseph Radding <raddingy@lykeme.com>
2015-02-07 23:04:04 -0500

    Fiixed max val error

--- turnedOn/backendStuffs/models.py
+++ turnedOn/backendStuffs/models.py
@@ -14,7 +14,7 @@ class Regions(models.Model):
 
 class UserPhone(models.Model):
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex], unique=True)
+	phone_number = models.CharField(validators=[phone_regex], unique=True, max_length = 10)
 	verificationNumber = models.IntegerField()
 	token = models.IntegerField()
 	region = models.ForeignKey(Regions)
@@ -27,6 +27,6 @@ class UserinGroup(models.Model):
 	description = models.TextField(blank = True)
 
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	twilioNumber = models.CharField(validators=[phone_regex])
+	twilioNumber = models.CharField(validators=[phone_regex],max_length = 10)
 
 	
